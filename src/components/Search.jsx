fa6d4ef53b77327f724d13e6bcb43f3a81bb1845
reynaldomarchell <reynaldomarchellba@gmail.com>
2023-10-28 16:00:35 +0700

    feat: add useRef hook in Search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,4 +1,27 @@
+import { useEffect, useRef } from "react";
+
 export default function Search({ query, setQuery }) {
+  const inputEl = useRef(null);
+
+  useEffect(() => {
+    const callback = (e) => {
+      if (document.activeElement === inputEl.current) {
+        if (e.code === "Escape") {
+          inputEl.current.blur();
+          setQuery("");
+        } else if (e.code === "Enter") {
+          inputEl.current.blur();
+        }
+        return;
+      } else if (e.code === "Enter") {
+        inputEl.current.focus();
+      }
+    };
+
+    document.addEventListener("keydown", callback);
+    return () => document.removeEventListener("keydown", callback);
+  }, [setQuery]);
+
   return (
     <input
       className="search"
@@ -6,6 +29,7 @@ export default function Search({ query, setQuery }) {
       placeholder="Search movies..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref={inputEl}
     />
   );
 }
